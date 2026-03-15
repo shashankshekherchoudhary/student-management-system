07790b8f4661d570d5394c5b1a1778d6d1ec61d8
Shashank <cshashankshekher@gmail.com>
2026-03-15 18:16:47 +0530

    Add marks validation feature

--- studentResultManagementSystem.py
+++ studentResultManagementSystem.py
@@ -37,8 +37,13 @@ def add_student():
     subjects = ['Physics','Chemistry','Mathematic']
     marks =[]
     for sub in subjects:
-        mark = int(input(f"Enter marks for {sub} out of 100 : "))
-        marks.append(mark)
+        while True:
+            mark = int(input(f"Enter marks for {sub} out of 100 : "))
+            if mark < 0 or mark > 100:
+                print("Marks should be between 0 and 100")
+                continue
+            marks.append(mark)
+            break
 
     total_marks = total(marks)
     percentagee = percentage(total_marks)
